67d506412ab4ee3d6d7ece0933d281d5411bf4be
Vitor Onoue <vitork.onoue@gmail.com>
2024-05-30 12:24:46 -0300

    desafio.py done

--- desafio.py
+++ desafio.py
@@ -18,13 +18,33 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print()
+        valor = float(input())
+        if valor < 0:
+            print()
+        else:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+        
 
     elif opcao == "s":
-        print()
+        valor = float(input())
+        if valor > limite:
+            print("saques podem ser de até 500, saque não efetuado")
+        else:
+            numero_saques += 1
+            if numero_saques > LIMITE_SAQUES:
+                print("limite de saques excedido, saque não efetuado")
+            elif valor > saldo:
+                print("saldo insuficiente, saque não efetuado")
+            elif valor < 0:
+                print()
+            else:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
 
     elif opcao == "e":
-        print()
+        print(extrato)
+        print(f"R$ {saldo:.2f}")
 
     elif opcao == "q":
         break
