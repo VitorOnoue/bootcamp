170d305ee413e466c1fc4e2078dc4ec27720aaf1
Vitor Onoue <vitork.onoue@gmail.com>
2024-06-12 14:25:34 -0300

    6/12 2:30pm

--- desafio.py
+++ desafio.py
@@ -1,18 +1,3 @@
-menu = """
-
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-
-=> """
-
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
-
 """keyword only saldo, valor, extrato, limite, numero_saques, limite_saques - saldo, extrato"""
 def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saldo = valor > saldo
@@ -60,12 +45,19 @@ def ver_extrato(saldo, /, *, extrato):
 
 """armazenar users em uma lista - usuario = nome, nascimento, cpf, endereço - endereço = logradouro, numero - bairro - cidade/sigla do estado - nao é permitido usuarios com o mesmo cpf"""
 def new_user(nome, nasc, cpf, end, users):
-    for tup in users:
-        if tup[2] == cpf:
-            return
     new_user = (nome, nasc, cpf, end)
 
     return new_user
+
+def user_already_exists(cpf, users):
+    for user in users:
+        if user[2] == cpf:
+            return True
+    return False
+
+def show_users(users):
+    for user in users:
+        print(f"Nome: {user[0]}\nData de nascimento: {user[1]}\nCPF: {user[2]}\nEndereço: {user[3]}\n")
 
 """armazenar contas em uma lista - conta = agencia, numero e user - numero da conta começa em 1, e é sequencial. agencia é sempre "0001" - usuario pode ter mais de uma conta"""
 def new_account(agencia, cpf, accounts, users):
@@ -75,52 +67,79 @@ def new_account(agencia, cpf, accounts, users):
             return new_account
     return
 
+def show_accounts(accounts):
+    for account in accounts:
+        print(f"Agência: {account[0]}\nNúmero: {account[1]}\nUsuário: {account[2]}\n")
 
-while True:
-
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
     users = []
     accounts = []
+    while True:
+        menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[u] Novo usuário
+[a] Nova conta
+[lu] Listar usuários
+[lc] Listar contas
+[q] Sair
+=> """
 
-    opcao = input(menu)
+        opcao = input(menu)
 
-    if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
-        saldo, extrato = depositar(saldo, valor, extrato)
-    
-    elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
-        saldo, extrato = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques, limite_saques=LIMITE_SAQUES)
-
-    elif opcao == "e":
-        ver_extrato(saldo, extrato=extrato)
-
-    elif opcao == "u":
-        nome = input("Informe seu nome: ")
-        nasc = input("Informe sua data de nascimento: ")
-        cpf = int(input("Informe seu cpf: "))
-
-        rua = input("Logradouro: ")
-        num_lar = int(input("Número da residência: "))
-        bairro = input("Bairro: ")
-        cidade = input("Cidade: ")
-        sigla = input("Sigla do estado: ")
-
-        endereco = f"{rua}, {num_lar} - {bairro} - {cidade}/{sigla}"
-
-        user = new_user(nome, nasc, cpf, endereco)
-
-        if user is not None:
-            users.append(user)
-
-    elif opcao == "a":
-        cpf = int(input("Informe seu cpf: "))
-        account = new_account("0001", cpf, accounts, users)
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
+            saldo, extrato = depositar(saldo, valor, extrato)
         
-        if account is not None:
-            accounts.append(account)
+        elif opcao == "s":
+            valor = float(input("Informe o valor do saque: "))
+            saldo, extrato = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques, limite_saques=LIMITE_SAQUES)
 
-    elif opcao == "q":
-        break
+        elif opcao == "e":
+            ver_extrato(saldo, extrato=extrato)
 
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        elif opcao == "u":
+            cpf = int(input("Informe seu cpf: "))
+            if user_already_exists(cpf, users):
+                print("Usuário com esse cpf já existe")
+
+            else:
+                nome = input("Informe seu nome: ")
+                nasc = input("Informe sua data de nascimento: ")
+
+                rua = input("Logradouro: ")
+                num_lar = int(input("Número da residência: "))
+                bairro = input("Bairro: ")
+                cidade = input("Cidade: ")
+                sigla = input("Sigla do estado: ")
+
+                endereco = f"{rua}, {num_lar} - {bairro} - {cidade}/{sigla}"
+
+                users.append(new_user(nome, nasc, cpf, endereco, users))
+
+        elif opcao == "a":
+            cpf = int(input("Informe seu cpf: "))
+            if not user_already_exists(cpf, users):
+                print("Não há usuários com esse cpf, impossível criar conta")
+            else:
+                accounts.append(new_account(AGENCIA, cpf, accounts, users))
+
+        elif opcao == "q":
+            break
+
+        elif opcao == "lu":
+            show_users(users)
+
+        elif opcao == "lc":
+            show_accounts(accounts)
+
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+main()
